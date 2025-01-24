2e67a1353cdf9376df308e9fee7f4320343a4aa5
susruthi1215 <susruthi1215@gmail.com>
2025-01-24 17:31:46 +0530

    first commit

---
+++ trafficrules.py
@@ -0,0 +1,5 @@
+# 1. A traffic police officer calculates fines based on the violation:
+# • Crossing red light: ₹500.
+# • Speeding: ₹1000.
+# • No helmet: ₹300.
+# Write a program to calculate the total fine based on user input
